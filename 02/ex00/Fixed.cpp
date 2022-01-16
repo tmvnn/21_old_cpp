377bb2cd71282db256685f5e59f7b797503b8bc1
Тимур Якубов <tmyakubov@bk.ru>
2022-01-16 20:09:44 +0300

    02_ex00_initial_push

---
+++ 02/ex00/Fixed.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbellona <lbellona@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/22 23:13:49 by lbellona          #+#    #+#             */
+/*   Updated: 2022/01/16 19:57:17 by lbellona         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+Fixed::Fixed()
+{
+	std::cout << "New Fixed is raised!" << std::endl;
+}
+
+Fixed::Fixed(int val_)
+{
+	this->val = val_;
+	std::cout << " Val = " << this->val << std::endl;
+}
+
+Fixed::Fixed(Fixed const &f_instance)
+{
+	this->val = f_instance.val;
+	std::cout << "From copy constructor Val = " << this->val << std::endl;
+}
+
+Fixed::~Fixed()
+{
+}
---
+++ 02/ex00/Fixed.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbellona <lbellona@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/22 23:07:05 by lbellona          #+#    #+#             */
+/*   Updated: 2022/01/16 19:48:37 by lbellona         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+# include <string>
+
+class Fixed
+{
+private:
+		int		val;
+public:
+		Fixed();
+		Fixed(int val_);
+		Fixed(Fixed const &f_instance);
+		~Fixed();
+
+		//void	announce( void );
+};
+
+#endif
