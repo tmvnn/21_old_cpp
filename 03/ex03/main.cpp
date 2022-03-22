f246a22ed11d5daa110bf99626c5309217fc284e
tmvnn <tmyakubov@bk.ru>
2022-03-22 23:02:19 +0300

    some changes

--- 03/ex03/main.cpp
+++ 03/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lbellona <lbellona@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/18 18:13:15 by lbellona          #+#    #+#             */
-/*   Updated: 2022/03/20 20:30:15 by lbellona         ###   ########.fr       */
+/*   Updated: 2022/03/22 23:01:06 by lbellona         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,18 @@
 int	main(void)
 {
 	DiamondTrap war1("warrior_1");
+	FragTrap war2("warrior_2");
+	
 	war1.whoAmI();
-	//FragTrap war2("warrior_2");
+	
 	war1.attack("warrior_2");
 	war1.takeDamage(45);
-	
+	war1.whoAmI();
 	war1.guardGate();
 	war1.highFivesGuys();
 	
 	
-	/*
+	
 	war1.setAttackDamage(12);
 	war1.attack("warrior_2");
 	war2.takeDamage(war1.getAttackDamage());
@@ -33,8 +35,7 @@ int	main(void)
 	war1.takeDamage(war2.getAttackDamage());
 	war2.highFivesGuys();
 
-	war1.whoAmI();
-	*/
-	war1.whoAmI();
+	
+	
 	return (0);
 }
